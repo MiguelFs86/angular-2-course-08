759d3f1a9fd2878c89c0eebf8f32b0b98cab7872
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2018-01-07 15:01:54 +0100

    <feature> All chapter 8

---
+++ src/app/components/data/data.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styles: []
+})
+export class DataComponent{
+
+	forma:FormGroup;
+	usuario:Object = {
+		nombrecompleto: {
+			nombre: "fernando",
+			apellido: "herrera"
+		},
+		correo: "mail@sample.com"
+	}
+
+	constructor() { 
+
+		console.log(this.usuario);
+
+		this.forma = new FormGroup({
+
+			'nombrecompleto': new FormGroup({
+				'nombre': new FormControl('', 
+					[Validators.required, 
+					Validators.minLength(3)]),
+				'apellido': new FormControl('', Validators.required),
+			}),			
+			'correo': new FormControl('', 
+				[Validators.required, 
+				Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+		});
+
+		this.forma.setValue(this.usuario);
+
+	}
+
+	guardarCambios(){
+		console.log(this.forma.value);
+		this.forma.setValue(this.usuario);
+		this.forma.reset();
+	}
+}
